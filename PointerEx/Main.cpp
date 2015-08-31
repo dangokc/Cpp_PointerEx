49979f95a1210b9c6a03a8157c33dd6403516db2
HuyDang <dangokc@gmail.com>
2015-08-31 16:15:41 -0500

    Array of Structs

--- PointerEx/Main.cpp
+++ PointerEx/Main.cpp
@@ -8,12 +8,24 @@ int main() {
 	struct student {
 		string name;
 		int age;
+		string toString() {
+			return name + "-" + to_string(age);
+		}
 	};
 
 	student s1 = { "huy", 32 };
 
-	cout << "Name: " << s1.name << endl;
-	cout << "Age " << s1.age << endl;
+	cout << "s1: " << s1.toString() << endl;
+
+	student arrayOfStudents[] = { 
+									{"John",20},
+									{ "Jim",21 },
+									{ "Jake",22 }
+	};
+
+	for (int i = 0; i < sizeof(arrayOfStudents) / sizeof(*arrayOfStudents); i++) {
+		cout << arrayOfStudents[i].toString() << endl;
+	}
 
 	//Pause the commandline
 	system("pause>nul");
