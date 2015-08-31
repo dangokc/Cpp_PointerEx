a301d4f170678b8159a209d066caf719bba2b2ec
HuyDang <dangokc@gmail.com>
2015-08-31 16:02:27 -0500

    Init struct branch

--- PointerEx/Main.cpp
+++ PointerEx/Main.cpp
@@ -5,39 +5,15 @@ using namespace std;
 
 int main() {
 	
-	const int SIZE_A = 6;
-	int a[SIZE_A];
-	cout << "a has: " << sizeof(a)/sizeof(*a) << " elements: " << endl;
+	struct student {
+		string name;
+		int age;
+	};
 
-	for (int i = 0; i < SIZE_A; i++) {
-		a[i] = i;
-		cout << "a[" << i << "] = " << i << endl;
-	}
+	student s1 = { "huy", 32 };
 
-	int* b = a;
-	int* c = a + 2;
-
-	cout << "\nb = a = " << *b << endl;
-	cout << "c = a+2 = " << *c << endl;
-
-	cout << "\nnow change value of b and c (*b = 100; *c = 500) " << endl;
-	*b = 100;
-	*c = 500;
-
-	for (int i = 0; i < SIZE_A; i++) {
-		cout << "a[" << i << "] = " << a[i] << endl;
-	}
-
-
-
-	
-
-
-
-
-
-
-
+	cout << "Name: " << s1.name << endl;
+	cout << "Age " << s1.age << endl;
 
 	//Pause the commandline
 	system("pause>nul");
